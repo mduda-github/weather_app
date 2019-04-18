4ad54e06116b27557037e9a79e9f50dc90b5e7e9
Marcin Duda <marc.duda94@gmail.com>
2019-04-19 00:18:18 +0200

    Updates

---
+++ precache-manifest.f1c08f1472a66b68060bbad1be74304f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "7ebb4c5cbdeae0135a96",
+    "url": "/weather-app/static/js/main.ba5a5056.chunk.js"
+  },
+  {
+    "revision": "aa570ab4875ccd9e7d22",
+    "url": "/weather-app/static/js/2.117e6e82.chunk.js"
+  },
+  {
+    "revision": "7ebb4c5cbdeae0135a96",
+    "url": "/weather-app/static/css/main.4be4e358.chunk.css"
+  },
+  {
+    "revision": "251ab80982aea3c086a6ed6a919a4740",
+    "url": "/weather-app/index.html"
+  }
+];
